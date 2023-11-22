da3bdf9d930076893298bc1275a26101e3444669
apsuadwf <1347115286@qq.com>
2023-11-22 17:09:47 +0800

    增加保存秒杀库存到redis中

--- src/main/java/com/hmdp/service/impl/VoucherServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/VoucherServiceImpl.java
@@ -2,16 +2,20 @@ package com.hmdp.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.hmdp.dto.Result;
+import com.hmdp.entity.SeckillVoucher;
 import com.hmdp.entity.Voucher;
 import com.hmdp.mapper.VoucherMapper;
-import com.hmdp.entity.SeckillVoucher;
 import com.hmdp.service.ISeckillVoucherService;
 import com.hmdp.service.IVoucherService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 import java.util.List;
+
+import static com.hmdp.utils.RedisConstants.SECKILL_STOCK_KEY;
 
 /**
  * <p>
@@ -27,6 +31,9 @@ public class VoucherServiceImpl extends ServiceImpl<VoucherMapper, Voucher> impl
     @Resource
     private ISeckillVoucherService seckillVoucherService;
 
+    @Autowired
+    private StringRedisTemplate redisTemplate;
+
     @Override
     public Result queryVoucherOfShop(Long shopId) {
         // 查询优惠券信息
@@ -36,7 +43,7 @@ public class VoucherServiceImpl extends ServiceImpl<VoucherMapper, Voucher> impl
     }
 
     @Override
-    @Transactional
+    @Transactional(rollbackFor =Exception.class)
     public void addSeckillVoucher(Voucher voucher) {
         // 保存优惠券
         save(voucher);
@@ -47,5 +54,7 @@ public class VoucherServiceImpl extends ServiceImpl<VoucherMapper, Voucher> impl
         seckillVoucher.setBeginTime(voucher.getBeginTime());
         seckillVoucher.setEndTime(voucher.getEndTime());
         seckillVoucherService.save(seckillVoucher);
+        // 保存秒杀库存到redis中
+        redisTemplate.opsForValue().set(SECKILL_STOCK_KEY + seckillVoucher.getVoucherId(),seckillVoucher.getStock().toString());
     }
 }
